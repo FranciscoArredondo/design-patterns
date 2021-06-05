f727d0410289a39731df621e5776e685eb8addc0
Francisco Arredondo <frarredo@icloud.com>
2021-06-05 12:51:40 -0700

    Added all the garage door command implementations.

---
+++ src/06 Command Pattern/Commands/GarageDoor/GarageDoorUpCommand.ts
@@ -0,0 +1,14 @@
+import Command from "../../Command";
+import GarageDoor from "../../Vendors/GarageDoor";
+
+export default class GarageDoorUpCommand implements Command {
+  private garageDoor: GarageDoor;
+
+  constructor(garageDoor: GarageDoor) {
+    this.garageDoor = garageDoor;
+  }
+
+  execute(): void {
+    this.garageDoor.up();
+  }
+}
