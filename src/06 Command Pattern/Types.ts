6f755959960fb3d1d8170f3751a5d53df2ecd5c0
Francisco Arredondo <frarredo@icloud.com>
2021-06-05 12:29:44 -0700

    Added a GarageDoorState type.
    A garage door consists of a door and light, so the GarageDoorState type is composed of these two items.

--- src/06 Command Pattern/Types.ts
+++ src/06 Command Pattern/Types.ts
@@ -2,3 +2,14 @@ export enum State {
   "On",
   "Off",
 }
+
+export enum DoorState {
+  "Up",
+  "Down",
+  "Stopped",
+}
+
+export type GarageDoorState = {
+  doorState: DoorState;
+  lightState: State;
+};
