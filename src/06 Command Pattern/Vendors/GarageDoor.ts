028afc5e4cb045cae6d88fb607dc720c68ce263d
Francisco Arredondo <frarredo@icloud.com>
2021-06-05 12:39:51 -0700

    Added some logs print outs to garage door actions.

--- src/06 Command Pattern/Vendors/GarageDoor.ts
+++ src/06 Command Pattern/Vendors/GarageDoor.ts
@@ -11,22 +11,27 @@ export default class GarageDoor {
   }
 
   up() {
+    console.log(`Garage door is going ${DoorState[this.state.doorState]}`);
     this.state.doorState = DoorState.Up;
   }
 
   down() {
+    console.log(`Garage door is going ${DoorState[this.state.doorState]}`);
     this.state.doorState = DoorState.Down;
   }
 
   stop() {
+    console.log(`Garage door is ${DoorState[this.state.doorState]}`);
     this.state.doorState = DoorState.Stopped;
   }
 
   lightOn() {
+    console.log(`The Garage light is now ${LightState[this.state.lightState]}`);
     this.state.lightState = LightState.On;
   }
 
   lightOff() {
+    console.log(`The Garage light is now ${LightState[this.state.lightState]}`);
     this.state.lightState = LightState.Off;
   }
 
