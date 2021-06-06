095d25f00fc8ca6072ec37719f63a2477e426b4e
Francisco Arredondo <frarredo@icloud.com>
2021-06-05 18:10:15 -0700

    Added CeilingFan commands.

---
+++ src/06 Command Pattern/Commands/CeilingFan/CeilingFanLowCommand.ts
@@ -0,0 +1,37 @@
+import Command from "../../Command";
+import { FanSpeed } from "../../Types";
+import CeilingFan from "../../Vendors/CeilingFan";
+
+export default class CelingFanLowCommand implements Command {
+  private ceilingFan: CeilingFan;
+  private prevState: FanSpeed;
+
+  constructor(ceilingFan: CeilingFan) {
+    this.ceilingFan = ceilingFan;
+    this.prevState = ceilingFan.getSpeed();
+  }
+
+  execute(): void {
+    this.prevState = this.ceilingFan.getSpeed();
+    this.ceilingFan.low();
+  }
+
+  undo(): void {
+    switch (this.prevState) {
+      case FanSpeed.High:
+        this.ceilingFan.high();
+        break;
+      case FanSpeed.Medium:
+        this.ceilingFan.medium();
+        break;
+      case FanSpeed.Low:
+        this.ceilingFan.low();
+        break;
+      case FanSpeed.Off:
+        this.ceilingFan.off();
+      default:
+        this.ceilingFan.off();
+        break;
+    }
+  }
+}
